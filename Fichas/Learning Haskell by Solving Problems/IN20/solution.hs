7876a24cf8c0ff526cd5fa16d10bcf459e2f9b72
Pedro Macedo <pedronunomacedo@hotmail.com>
2022-09-18 07:30:12 +0100

    Solved IN20

---
+++ Fichas/Learning Haskell by Solving Problems/IN20/solution.hs
@@ -0,0 +1,13 @@
+-- isPrime :: Integral n => n -> Bool
+
+isPrime n
+  | (n <= 1)       = False
+  | (n == 2)       = True
+  | (mod n 2 == 0) = False
+  | otherwise      = isDivisorOfn 1 n 0
+
+isDivisorOfn t n numTimes
+  | (t > n && numTimes /= 2) = False
+  | (t > n && numTimes == 2)  = True
+  | ((mod n t) == 0)          = isDivisorOfn (t + 1) n (numTimes + 1)
+  | otherwise                 = isDivisorOfn (t + 1) n numTimes
