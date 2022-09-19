78297031d1ca7a8e846fb166cdd5c5cfb40c6d3c
Pedro Macedo <pedronunomacedo@hotmail.com>
2022-09-19 11:28:14 +0100

    solved FT20

---
+++ Fichas/Learning Haskell by Solving Problems/2. FT/FT20/solution.hs
@@ -0,0 +1,26 @@
+-- a)
+  -- i)
+  ['a','b','c'] :: [Char]
+
+  -- ii)
+  "abc" :: [Char]
+
+  -- iii)
+  42 :: Integer
+
+  -- iv)
+  42.5 :: Double
+
+  -- v)
+  [(False,1),(True,3.5)] :: [(Bool, Float)]
+
+  -- vi)
+  reverse [(False,1),(True,3.5)] :: [(Bool,Float)]
+
+  -- vii)
+  ([1],[1.5,2.5]) :: ([Int], [Float])
+
+  -- viii)
+  tail [1] :: Num a => [a]
+
+-- b) ????
