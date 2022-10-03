0e94f7249c0ccb49b6c3d0e3887f6b97b817c830
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-10-04 00:03:34 +0100

    Continued F3 (Practical classes)

--- Fichas/Practical Classes/F3 - Funções de ordem superior/F3_solution.hs
+++ Fichas/Practical Classes/F3 - Funções de ordem superior/F3_solution.hs
@@ -16,3 +16,23 @@ myZipWith f (x:xs) (y:ys) = (f x y):(myZipWith f xs ys)
 -- 3.4
 isort :: Ord a => [a] -> [a]
 isort l = foldr (\x y -> (insert x y)) [] l
+
+-- 3.5 a)
+myMaximum :: Ord a => [a] -> a
+myMaximum [] = error "The list needs to have at least one element!"
+myMaximum l = foldl (\x y -> (if (x >= y) then x else y) ) (head l) l
+
+myMinimum :: Ord a => [a] -> a
+myMinimum [] = error "The list needs to have at least one element!"
+myMinimum l = foldr (\x y -> (if (x <= y) then x else y) ) (head l) l
+
+-- 3.5 b)
+myFoldl1 :: Ord a => (a -> a -> a) -> [a] -> a
+myFoldl1 f l = foldl f (head l) (tail l)
+
+myFoldr1 :: (b -> b -> b) -> [b] -> b
+myFoldr1 f l = foldr f (last l) (init l)
+
+-- 3.6
+mdc :: (a -> Bool) -> (a -> a) -> a -> a
+mdc a b =
