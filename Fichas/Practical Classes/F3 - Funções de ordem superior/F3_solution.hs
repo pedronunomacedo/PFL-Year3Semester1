a92d15309be160a91a8d098018a101e59c5de0d5
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-10-10 15:57:16 +0100

    Done in the class 10/10/2022

--- Fichas/Practical Classes/F3 - Funções de ordem superior/F3_solution.hs
+++ Fichas/Practical Classes/F3 - Funções de ordem superior/F3_solution.hs
@@ -20,7 +20,7 @@ isort l = foldr (\x y -> (insert x y)) [] l
 -- 3.5 a)
 myMaximum :: Ord a => [a] -> a
 myMaximum [] = error "The list needs to have at least one element!"
-myMaximum l = foldl (\x y -> (if (x >= y) then x else y) ) (head l) l
+myMaximum l = foldl (\x y -> (if (x >= y) then x else y)) (head l) l
 
 myMinimum :: Ord a => [a] -> a
 myMinimum [] = error "The list needs to have at least one element!"
@@ -34,5 +34,31 @@ myFoldr1 :: (b -> b -> b) -> [b] -> b
 myFoldr1 f l = foldr f (last l) (init l)
 
 -- 3.6
-mdc :: (a -> Bool) -> (a -> a) -> a -> a
-mdc a b =
+mdc :: Int -> Int -> Int
+mdc a b = fst (until (\(a,b) -> b == a)
+                      (\(a,b) -> if (a > b)
+                          then (a - b, b)
+                          else (a, b - a)
+                      )
+                      (a,b)
+              )
+
+-- 3.7 a)
+twoPlus :: [a] -> [a] -> [a]
+twoPlus x y = foldr (:) y x
+
+-- 3.7 b)
+myConcat :: [[a]] -> [a]
+myConcat l = foldr (++) [] l
+
+-- 3.7 c)
+myReverse1 :: [a] -> [a]
+myReverse1 l = foldr (\x y -> y ++ [x]) [] l
+
+-- 3.7 d)
+myReverse2 :: [a] -> [a]
+myReverse2 l = foldl (\x y -> [y] ++ x) [] l
+
+-- 3.7 e)
+myElem :: Eq a => a -> [a] -> Bool
+myElem n l = any (n==) l
